2a7269bf7fcf39458e0a6acb497ae0eecc58b406
Graham Picard <grahampicard@gmail.com>
2020-05-26 11:25:41 -0400

    fix for heroku

--- static/js/modalbox.js
+++ static/js/modalbox.js
@@ -14,9 +14,9 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0]
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal.style.display = "none"
 }
